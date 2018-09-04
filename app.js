eaa37da042c709bf997cafd7ceeb828b7850ebf1
Tomás Amaro <tomascorreiamaro@gmail.com>
2018-09-04 12:54:47 +0100

    Removed cors protection

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const logger = require('morgan');
 const config = require ('./config');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const cors = require('cors');
 
 const apiRouter = require('./api');
 
@@ -15,16 +16,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(passport.initialize());
+app.use(cors());
 app.use('/api', apiRouter);
 mongoose.connect(config.dbPath);
 
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error: '));
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
 
 app.listen(config.PORT, () => {
     console.log(`Server running on port: ${config.PORT}`);
