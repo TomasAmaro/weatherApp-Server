4408c8c0011c3b3d0a69539ed8a70e88fe59e6b4
Tomás Amaro <tomascorreiamaro@gmail.com>
2018-09-04 12:55:06 +0100

    Deal with errors from original api

--- controllers/weather.controller.js
+++ controllers/weather.controller.js
@@ -4,29 +4,30 @@ const superagent = require('superagent');
 
 weatherController.getWeatherByCity = (req, res, next) => {
     superagent.get(config.weatherAPI)
-    .query({
-        q: req.query.location,
-        units: req.query.units !== 'undefined' ? req.query.units : 'metric',
-        APPID: config.weatherAPIKey,
-    }).end((err, response) => {
-        if (err) {
-            res.status(500).send('We are having troubles in the main api');
-        }
-        const apiResponse = JSON.parse(response.text);
-        const weatherCode = getWeatherCode(apiResponse.weather[0].id);
-        const responseToSend = {
-            location: apiResponse.name,
-            generalInformation: apiResponse.main,
-            wind: apiResponse.wind,
-            description: apiResponse.weather[0].description,
-            code: weatherCode,
-        }
-        res.status(200).send(responseToSend);
-    })
+        .query({
+            q: req.query.location,
+            units: req.query.units !== 'undefined' ? req.query.units : 'metric',
+            APPID: config.weatherAPIKey,
+        }).end((err, response) => {
+            if (err) {
+                res.status(err.status).send('We are having troubles in the main api');
+            } else {
+                const apiResponse = JSON.parse(response.text);
+                const weatherCode = getWeatherCode(apiResponse.weather[0].id);
+                const responseToSend = {
+                    location: apiResponse.name,
+                    generalInformation: apiResponse.main,
+                    wind: apiResponse.wind,
+                    description: apiResponse.weather[0].description,
+                    code: weatherCode,
+                }
+                res.status(200).send(responseToSend);
+            }
+        })
 }
 
 function getWeatherCode(code) {
-    if(code > 800) {
+    if (code > 800) {
         return 9;
     } else {
         return Number(String(code).charAt(0));
